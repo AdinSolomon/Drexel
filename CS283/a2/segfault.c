48771ab046818d9bbf745abd48373d5445d8f0a9
AdinSolomon <adinbsolomon@gmail.com>
2020-07-23 19:45:32 -0400

    CS283/a2

--- CS283/a2/segfault.c
+++ CS283/a2/segfault.c
@@ -20,17 +20,23 @@ void fib(int* A, int n);
 
 int
 main(int argc, char *argv[]) {
+	// P1
 	int buf[10];
 	unsigned int i;
+	// P2
 	char *str;
 	char *printThisOne;
+	// P3
 	char *word;
+	// P4
 	int *integers;
+	// P8
 	int foo;
+	// P9
 	int *bar;
+	// P10
 	char *someText;
 
-	printf("P1\n");
 	// P1
 	for (i = 0; i < 10; ++i) {
 		buf[i] = i;
@@ -39,92 +45,86 @@ main(int argc, char *argv[]) {
 		printf("Index %d = %d\n", i, buf[i]);
 	}
 	/* Error:
-		Compiling Error:
-			printf had the wrong formatting for its arguments
-		Runtime Error:
-			the for loops were extending to buf[10] but buf[9] was the last word allocated
+		Printf had the wrong formatting for its arguments
+		The for loops were extending to buf[10] but buf[9] was the last word allocated
 	 */
-	printf("P2\n");
 	// P2
-	str = malloc(sizeof(char) * 1000);
+	str = malloc(sizeof(char) * 19);
 	strcpy(str, "Something is wrong");
-	printf("%s\n", printThisOne);
-	/* Error:
-		the second argument in strcpy (including the null character) is longer than 10, the original number of character malloced into str
+	printf("%s\n", printThisOne = str);
+	/* Error
+		We need more than 10 bytes for str (18 chars + 1 null char)
+		In order to print the string pointed to by str while technically printing the printThisOne variable, we need to have printThisOne point to the same memory location
 	 */
 
-	printf("P3\n");
 	// P3
-	word = "Part 3....";
+	word = malloc(sizeof(char) * 7);
+	strcpy(word, "Part 3");
 	*(word + 4) = '-';
 	printf("%s\n", word);
+	free(word);
 	/* Error:
-
+		Bc the string pointed to by word could be in read-only memory (in this case it is), we need to make sure it's stored in the heap instead
 	 */
 
-	printf("P4\n");
 	// P4
+	integers = malloc(sizeof(int) * 11);
 	*(integers + 10) = 10;
 	printf("Part 4: %d\n", *(integers + 10));
 	free(integers);
 	/* Error:
-
+		Integers was not assigned to an allocated spot in memory
+		Alternatively the declaration could include an array of size at least 11
 	 */
 
-	printf("P5\n");
 	// P5
 	printf("Print this whole line\n");
 	/* Error:
-		The null character was in the string
+		The null character was in the string so the string which is a no no
 	 */
 
-	printf("P6\n");
-	// P6
-	int x;	// bad style but it's only for this section
+	long int x;
 	x = 2147483647;
-	printf("%d is positive\n", x); 
+	printf("%ld is positive\n", x); 
 	x += 1000000000;
-	printf("%d is positive\n", x); 
+	printf("%ld is positive\n", x); 
 	/* Error:
-	 	Compilling Error:
-			x was never initialized
-		Runtime Error:
-			
+		x was never initialized
+		x needs to be a long bc after the addition there would be an overflow error
 	 */
 
-	printf("P7\n");
 	// P7
 	printf("Cleaning up memory from previous parts\n");
 	free(str);
+	//free(buf)
 	/* Error:
 		buf is on the stack so it can't be freed
 	 */
 
-	printf("P8\n");
 	// P8
 	fib(&foo, 7);
 	printf("fib(7) = %d\n", foo);
 	/* Error:
-		Compilling Error:
-			foo needed to be passed as a reference
+		foo needed to be passed as a reference
 	 */
 
-	printf("P9\n");
 	// P9
-	bar = 0;
+	//bar = 0;
+	bar = malloc(sizeof(int) * 1);
 	*bar = 123;
 	printf("bar = %d\n", *bar);
 	/* Error:
-
+		In setting bar to 0, we can't be sure that memory location 0 is allocated to this program
+		When compiling and running on local WSL Ubuntu, there were no errors with commenting out that line but on tux there was so I just added the malloc to handle it more explicitly
 	 */
 
 	// P10
 	someText = malloc(10);
 	strcpy(someText, "testing");
-	free(someText);
 	printf("someText = %s\n", someText);
+	free(someText);
 	/* Error:
-
+		We tried freeing someText before our last use of it!
 	 */
 
 	exit(0);
