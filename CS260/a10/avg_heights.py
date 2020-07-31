1f771bef2081d1bd68131d0d5a431ad3117f519d
AdinSolomon <adinbsolomon@gmail.com>
2020-07-30 22:36:38 -0400

    CS 260 Assignment 10

---
+++ CS260/a10/avg_heights.py
@@ -0,0 +1,22 @@
+import random
+from test_bst import *
+
+def gen(n):
+	nums = []
+	for i in range(n):
+		nums.append(random.randint(0, 5*n))
+	return make_tree(nums).height()
+
+if __name__ == "__main__":
+	print("Elements, Experiment 1, Experiment 2, Experiment 3, Epxeriment 4, Experiment 5, Average")
+	x = 16
+	for i in range(12):
+		n = x * 2**i
+		total = 0
+		row = str(n) + ","
+		for j in range(5):
+			exp = gen(n)
+			total += exp
+			row += str(exp) + ","
+		row += str(total / 5)
+		print(row)
